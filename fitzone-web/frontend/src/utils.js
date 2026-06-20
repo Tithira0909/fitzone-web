9ca5fd015002db7dc8e9d8f32ce1475c16af8553
Thiviru <thiviopallage@gmail.com>
2026-06-20 07:11:12 +0530

    Remove accidentally nested project folder

--- fitzone-web/frontend/src/utils.js
+++
@@ -1,7 +0,0 @@
-export function formatCurrency(value) {
-  return new Intl.NumberFormat('en-LK', { style: 'currency', currency: 'LKR', maximumFractionDigits: 0 }).format(Number(value || 0));
-}
-
-export function statusLabel(status) {
-  return String(status || '').replaceAll('_', ' ');
-}
